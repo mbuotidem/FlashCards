464cff30df8946b53e505441529e84dff11533e4
Mbuotidem Isaac <mbuotidemisaac@gmail.com>
2021-04-06 20:30:19 -0500

    Add Deck and Card models

---
+++ FlashCards.Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FlashCards.Models
+{
+    public class Card
+    {
+        public Card()
+        {
+
+        }
+
+        public Card(int cardId, string term, string definition)
+        {
+            CardId = cardId;
+            Term = term;
+            Definition = definition;
+            CreationTimeStamp = DateTime.UtcNow;
+        }
+        public int CardId { get; set; }
+
+        [Required]
+        [ForeignKey("Deck")]
+        public int DeckId { get; set; }
+
+        [ForeignKey("DeckId")]
+        public virtual Deck Deck { get; set; }
+
+        public string Term { get; set; }
+
+        public string Definition { get; set; }
+
+        public bool Correct { get; set; }
+        
+        public DateTime CreationTimeStamp { get; set; }
+    }
+}
