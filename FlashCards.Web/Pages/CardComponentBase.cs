7504b3072c2cf68bbf4b2d15837d576e00e44925
Sarah Mwema <ln3213fj@go.minnstate.edu>
2021-04-08 01:01:01 -0500

    added the on click and add button method , i am able to show and hide footer(definition or answer)

---
+++ FlashCards.Web/Pages/CardComponentBase.cs
@@ -0,0 +1,64 @@
+﻿using FlashCards.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashCards.Web.Pages
+{
+    public class CardComponentBase : ComponentBase
+    {
+        public List<Card> Cards { get; set; } = new List<Card>();
+
+        protected string ButtonText { get; set; } = "Hide Answer";
+        protected string CssClass { get; set; } = null;
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadCards();
+            return base.OnInitializedAsync();
+        }
+
+        protected void Button_Click()
+        {
+            if (ButtonText== "Hide Answer")
+            {
+                ButtonText = "Show Answer";
+                CssClass = "HideFooter";
+            }
+
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Answer";
+            }
+        }
+
+
+        private void LoadCards()
+        {
+            Card c1 = new Card
+            {
+                CardId = 1,
+                Term = "IT",
+                Definition = "Information Technology",
+                CreationTimeStamp = DateTime.UtcNow
+            };
+
+            Card c2 = new Card
+            {
+                CardId = 2,
+                Term = "CIS",
+                Definition = " Computer Information System",
+                CreationTimeStamp = DateTime.UtcNow
+            };
+            Cards.Add(c1);
+            Cards.Add(c2);
+
+        }
+
+
+    }
+
+}
